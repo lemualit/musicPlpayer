63936dc4cdec5ba84e69bd4e63ceb35b94251d25
lemualit <140512285+lemualit@users.noreply.github.com>
2023-09-23 21:50:25 +0700

    you
    you

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,10 @@
-const musicButton = document.querySelector('.music-button');
-const audio = new Audio('music/Whats.mp3');
-let isPlaying = false;
+const music = document.querySelector('#music');
+const seekBar = document.querySelector('#seek-bar');
 
-musicButton.addEventListener('click', function() {
-  if (isPlaying) {
-    audio.pause();
-    isPlaying = false;
-  } else {
-    audio.play();
-    isPlaying = true;
-  }
+music.addEventListener('timeupdate', () => {
+  seekBar.value = music.currentTime;
+});
+
+seekBar.addEventListener('input', () => {
+  music.currentTime = seekBar.value;
 });
