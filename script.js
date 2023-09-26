16bab78d7a87b3b633f6d4f75c13f1ee77df1f64
lemualit <140512285+lemualit@users.noreply.github.com>
2023-09-26 17:36:55 +0700

    Yo Hoo!

--- script.js
+++ script.js
@@ -4,20 +4,20 @@ const Stranger = new Audio('./sound/Stranger.mp3');
 const Castor = new Audio('./sound/Castor.mp3');
 const Sunny = new Audio('./sound/Sunny.mp3');
 
-// selecting elements
 const prevBtn = document.querySelector('.previous');
 const playBtn = document.querySelector('.play-pause');
 const nextBtn = document.querySelector('.next');
 const songName = document.querySelector('.song-name');
 const playPauseIcon = document.querySelector('#play-pause-icon');
+const btnLike = document.querySelector(".btnLike");
+const icolike = document.querySelector("#like")
 
-
-const songs = [
-  { ele: Sunflower, audioName: 'Sunflower', like: false },
-  { ele: Whats, audioName: "What's up danger?",  like: false  },
-  { ele: Stranger, audioName: 'Stranger things main theme',  like: false  },
-  { ele: Castor, audioName: 'Daft Punk - Castor',  like: false  },
-  { ele: Sunny, audioName: 'Bonney.M Sunny',  like: false },
+var songs = [
+  { id: 1, ele: Sunflower, audioName: 'Sunflower', like: false, bg: "./pict/Sunflower.png" },
+  { id: 2, ele: Whats, audioName: "What's up danger?",  like: false, bg: "./pict/Whats.png" },
+  { id: 3, ele: Stranger, audioName: 'Stranger things main theme',  like: false, bg:"green"},
+  { id: 4, ele: Castor, audioName: 'Daft Punk - Castor',  like: false, bg: "white"  },
+  { id: 5, ele: Sunny, audioName: 'Bonney.M Sunny',  like: false, bg: "black" },
 ];
 
 for(const song of songs) {
@@ -31,10 +31,18 @@ let current = 0;
 let currentSong = songs[current].ele;
 songName.textContent = songs[current].audioName;
 
+switch (songs[current].like) {
+  case true:
+      icolike.className = "fa-solid fa-heart"
+    break;
+  case false:
+      icolike.className = "fa-regular fa-heart"
+    break;
+}
 
-// const audio = (songs[current].ele);
-// const audioProgress = document.getElementById('progress');
+let picture = document.querySelector('#picture');
 
+picture.style.backgroundImage = 'url(./pict/Sunflower.png)';
 playBtn.addEventListener('click',()=> {
   playPauseSong();
 })
@@ -48,6 +56,26 @@ prevBtn.addEventListener('click', () => {
   updateSong('prev');
   playPauseSong();
 });
+
+btnLike.addEventListener('click', () => {
+  var songItem = {...songs[current], like: !songs[current].like }
+  switch (songItem.like) {
+    case true:
+        icolike.className = "fa-solid fa-heart"
+      break;
+    case false:
+        icolike.className = "fa-regular fa-heart"
+      break;
+  }
+  songs = songs.map(item => {
+    if(item.id == songItem.id){
+      return songItem;
+    }
+    return item;
+  })
+  console.log(songItem);
+  console.log(songs);
+})
 
 let progress = document.getElementById("progress");
 
@@ -67,7 +95,7 @@ progress.onchange = function(){
 const updateSong = (action)=> {
   currentSong.pause();
   currentSong.currentTime = 0;
-
+  
   if(action === 'next'){
     current++;
     if(current > songs.length -1) current = 0;
@@ -76,8 +104,20 @@ const updateSong = (action)=> {
     current--;
     if(current < 0) current = songs.length - 1;
   }
+  
   currentSong = songs[current].ele;
+
+  switch (songs[current].like) {
+    case true:
+        icolike.className = "fa-solid fa-heart"
+      break;
+    case false:
+        icolike.className = "fa-regular fa-heart"
+      break;
+  }
+
   songName.textContent = songs[current].audioName;
+  picture.style.backgroundImage = `url( ${songs[current].bg} )`;
 }
 
 const playPauseSong = ()=> {
@@ -90,6 +130,7 @@ const playPauseSong = ()=> {
     playPauseIcon.className = 'fa-solid fa-circle-play fa-xl bigger';
   }
 }
+
 
 
 
